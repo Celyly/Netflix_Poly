e0adca9be3c8eec35aa6b4e1d5e6ed78e7eb4ff2
Céline Ly <celine.ly@polymtl.ca>
2020-04-03 13:59:04 -0400

    Modified sql files

--- Cadriciel/bdschema.sql
+++ Cadriciel/bdschema.sql
@@ -4,7 +4,7 @@ DROP SCHEMA IF EXISTS NETFLIXDB CASCADE;
 CREATE SCHEMA NETFLIXDB;
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Member (
-	memberId		    SERIAL                  NOT NULL,
+	memberId		    VARCHAR(50)             NOT NULL,
     memberName          VARCHAR(50)             NOT NULL,
     memberPassword      VARCHAR(50)             NOT NULL, -- Doit être encrypté
     email               VARCHAR(50)             NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.Member (
 );
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.MemberMonthly (
-	memberId		    SERIAL                  NOT NULL,
+	memberId		    VARCHAR(50)             NOT NULL,
     subscriptionPrice   NUMERIC(9,2)            NOT NULL,
     startDate           DATE                    NOT NULL,
     dueDate             DATE                    NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.MemberMonthly (
 );
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.MemberPayPerView (
-	memberId		        SERIAL                NOT NULL,
+	memberId		        VARCHAR(50)           NOT NULL,
     film_payperview         INT                   NOT NULL,
     FOREIGN KEY (memberId) REFERENCES NETFLIXDB.Member(memberId)
     ON UPDATE CASCADE ON DELETE CASCADE,
@@ -32,8 +32,8 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.MemberPayPerView (
 
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.CreditCard (
-    cardNo              VARCHAR(20)          NOT NULL,
-	memberId	        SERIAL               NOT NULL,
+    cardNo              VARCHAR(50)          NOT NULL,
+	memberId	        VARCHAR(50)          NOT NULL,
     ccv                 INT                  NOT NULL,
     ownerCard           VARCHAR(50)          NOT NULL,
     expirationDate      DATE                 NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.CreditCard (
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Order (
     orderNo             SERIAL                NOT NULL,
-    memberId            SERIAL                NOT NULL,
+    memberId            VARCHAR(50)           NOT NULL,
     deliveryPrice       NUMERIC(9, 2)         NOT NULL,
     deliveryDate        DATE                  NOT NULL,
     -- distance            NUMERIC(9, 2)         NOT NULL,
@@ -55,19 +55,19 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.Order (
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Movie (
     movieNo         SERIAL                  NOT NULL,
-    title           VARCHAR(20)             NOT NULL,
-    genre           VARCHAR(20)             NOT NULL,
+    title           VARCHAR(50)             NOT NULL,
+    genre           VARCHAR(50)             NOT NULL,
     productionDate  DATE                    NOT NULL,
-    duration        TIME                    NOT NULL,
+    duration        INT                    NOT NULL,
     PRIMARY KEY (movieNo)
 );
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Viewing (
     viewNo          SERIAL               NOT NULL,
     movieNo         SERIAL               NOT NULL,
-    memberId        SERIAL               NOT NULL,
+    memberId        VARCHAR(50)          NOT NULL,
     viewDate        DATE                 NOT NULL,
-    duration        TIME                 NOT NULL,
+    duration        INT                 NOT NULL,
     FOREIGN KEY (memberId) REFERENCES NETFLIXDB.Member(memberId)
     ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (movieNo) REFERENCES NETFLIXDB.Movie(movieNo)
@@ -89,26 +89,25 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.DVD (
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Person (
     personId        SERIAL                  NOT NULL,
-    personName      VARCHAR(20)             NOT NULL,
+    personName      VARCHAR(50)             NOT NULL,
     dateNaissance   DATE                    NOT NULL,
     sex             CHAR                    DEFAULT 'M' CHECK (sex IN ('M', 'F')),
-    nationality     VARCHAR(20),
+    nationality     VARCHAR(50),
     PRIMARY KEY (personId)
 );
 
--- J'ai décidé de mettre salaire dans Role étant donné qu'on a une référence vers la personne concernée
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Role (
     roleNo          SERIAL                  NOT NULL,
     personId        SERIAL                  NOT NULL,
     roleNom         VARCHAR(50)             NOT NULL,
     salary          NUMERIC(9,2)            NOT NULL,
-    PRIMARY KEY (personId)
+    PRIMARY KEY (roleNo)
 );
 
 CREATE TABLE IF NOT EXISTS NETFLIXDB.Ceremony (
     ceremonyNo      SERIAL                  NOT NULL,
-    host            VARCHAR(20)             NOT NULL,
-    -- category        VARCHAR(20)             NOT NULL,
+    host            VARCHAR(50)             NOT NULL,
+    -- category        VARCHAR(50)             NOT NULL,
     PRIMARY KEY (ceremonyNo)
 );
 
@@ -116,8 +115,8 @@ CREATE TABLE IF NOT EXISTS NETFLIXDB.Oscar (
     oscarNo         SERIAL                  NOT NULL,
     ceremonyNo      SERIAL                  NOT NULL,
     movieNo         SERIAL                  NOT NULL,
-    category        VARCHAR(20)             NOT NULL,
-    oscarType       VARCHAR(20)             NOT NULL CHECK (oscarType IN ('Nominee','Winner')),
+    category        VARCHAR(50)             NOT NULL,
+    oscarType       VARCHAR(50)             NOT NULL CHECK (oscarType IN ('Nominee','Winner')),
     FOREIGN KEY (ceremonyNo) REFERENCES NETFLIXDB.Ceremony(ceremonyNo)
     ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (movieNo) REFERENCES NETFLIXDB.Movie(movieNo)
