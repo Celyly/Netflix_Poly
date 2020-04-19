a0b875ad62337b4a0c3a7049fd99c036e05937e5
Céline Ly <celine.ly@polymtl.ca>
2020-04-19 13:10:38 -0400

    Remove unused import

--- Cadriciel/INF3710_TP4-master/client/src/app/registration/register-plan/register-plan.component.ts
+++ Cadriciel/INF3710_TP4-master/client/src/app/registration/register-plan/register-plan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { LoggedUser } from "src/app/logged-user";
 import { RegisterMemberService } from "src/app/register-member.service";
 
